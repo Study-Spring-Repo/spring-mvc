f87167b9a1575e6109b0566b8bccf6a405f147c1
hyunseo <goatsfish@gmail.com>
2022-12-18 14:24:15 +0900

    [Spring-Type-Converter] : 스프링 컨버터 기본 예시

---
+++ spring-type-converter/src/main/java/com/example/springtypeconverter/controller/HelloController.java
@@ -0,0 +1,36 @@
+package com.example.springtypeconverter.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        // 문자 타입 조회
+        String data = request.getParameter("data");
+
+        // 숫자 타입으로 변경
+        Integer intValue = Integer.valueOf(data);
+
+        log.info("intValue = {}", intValue);
+
+        return "ok";
+    }
+
+    /**
+     * @RequestParam을 사용하면
+     * 문자 10을 Integer 타입의 숫자로 스프링이 중간에 변환해준다.
+     */
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        log.info("intValue = {}", data);
+
+        return "ok";
+    }
+}
