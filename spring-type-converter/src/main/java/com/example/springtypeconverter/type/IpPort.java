be6821d38d6abccb913c30cff9e55aaf389c54c6
hyunseo <goatsfish@gmail.com>
2022-12-18 15:18:44 +0900

    [Spring-Type-Converter] : ip, port 컨버터 예시

---
+++ spring-type-converter/src/main/java/com/example/springtypeconverter/type/IpPort.java
@@ -0,0 +1,17 @@
+package com.example.springtypeconverter.type;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+public class IpPort {
+
+    private String ip;
+    private int port;
+
+    public IpPort(String id, int port) {
+        this.ip = id;
+        this.port = port;
+    }
+}
--- spring-type-converter/src/test/java/com/example/springtypeconverter/converter/ConverterTest.java
+++ spring-type-converter/src/test/java/com/example/springtypeconverter/converter/ConverterTest.java
@@ -1,5 +1,6 @@
 package com.example.springtypeconverter.converter;
 
+import com.example.springtypeconverter.type.IpPort;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -18,5 +19,21 @@ public class ConverterTest {
         IntegerToStringConverter converter = new IntegerToStringConverter();
         String result = converter.convert(10);
         assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void string_to_ip_port() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8000");
+    }
+
+    @Test
+    void ip_port_to_string() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        String source = "127.0.0.1:8080";
+        IpPort result = converter.convert(source);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
     }
 }
